43695c0c7458b2f10896ab2426a15584aa24063a
Anastasiya Goeva <nastya.goeva@yandex.ru>
2020-12-11 01:24:05 +0300

    Initital commit

---
+++ Watch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Watch WatchKit Extension
+//
+//  Created by Анастасия on 11.12.2020.
+//  Copyright © 2020 Анастасия. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Watch WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Watch WatchKit Extension
+//
+//  Created by Анастасия on 11.12.2020.
+//  Copyright © 2020 Анастасия. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
